c87282a2781d827b0276989d08c8ab9bcf2ccd44
Bryan Lee <trgddgf@gmail.com>
2021-09-11 14:21:14 +0800

    deez

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { HeroContainer, HeroBg, ImageBg } from './HeroElements';
+// import Image from '../../../assets/landing';
 
 const Hero = () => {
   return (
     <HeroContainer>
       <HeroBg>
-        <ImageBg src="../../assets/landing/papaya.png"></ImageBg>
+        <ImageBg src="../../../assets/landing/papaya.png"></ImageBg>
       </HeroBg>
     </HeroContainer>
   );
